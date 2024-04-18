1e461bdcd72f40fbbd64b665be104707794ba0ae
David Mar <david.bryan.mar@gmail.com>
2024-04-17 23:01:47 -0500

    Update App.js

--- UserRegistrationLogin/frontend/src/App.js
+++ UserRegistrationLogin/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Auth0ProviderWithHistory from './auth/Auth0ProviderWithHistory';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Home from './components/Home'; // Assume a Home component for the main page
@@ -7,19 +8,22 @@ import Home from './components/Home'; // Assume a Home component for the main pa
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Auth0ProviderWithHistory>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Auth0ProviderWithHistory>
     </Router>
   );
 }
 
 export default App;
+
