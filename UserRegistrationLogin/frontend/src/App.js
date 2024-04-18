ede7b105bda0e05605406f7207315134c82abbbf
David Mar <david.bryan.mar@gmail.com>
2024-04-17 22:55:16 -0500

    Create App.js

---
+++ UserRegistrationLogin/frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Home from './components/Home'; // Assume a Home component for the main page
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
